e9b93c3c154130994f7312750d10ca1e7bba92dc
Robson Lopes <rob_lopes7@hotmail.com>
2022-06-03 13:54:48 -0300

    first commit

---
+++ src/main/java/com/projeto/acolhimento/AcolhimentoApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.acolhimento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcolhimentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcolhimentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/acolhimento/AcolhimentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.acolhimento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcolhimentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
